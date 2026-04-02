1c2c32189360098d6c5e355a53737fbf5987278a
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-04-02 16:56:42 +0000

    wads

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,26 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Evan
+Created on: Apirl 2026
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+# Setup: Clear screen, turn off pin 16, and show happy face
+display.clear()
+pin16.write_digital(0)
+display.show(Image.HAPPY)
+
+while True:
+    # Turn on LED when Button A is pressed
+    if button_a.was_pressed():
+        display.show(Image.YES)
+        pin16.write_digital(1)
+
+    # Turn off LED when Button B is pressed
+    if button_b.was_pressed():
+        display.show(Image.NO)
+        pin16.write_digital(0)
+
+    # Small sleep to save CPU power
+    sleep(10)
